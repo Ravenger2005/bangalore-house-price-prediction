5016cc9408ee94ea7febe9f3952590afe4fd0087
Shashank Panyam <shashank.panyam26@gmail.com>
2026-01-20 00:19:27 +0530

    Fixing static paths for Render

--- server/server.py
+++ server/server.py
@@ -1,24 +1,24 @@
-from  flask import Flask,request,jsonify,send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 import util
 import os
-app = Flask(__name__)
 
-# This tells Flask to look for your UI in the ../client folder
-app = Flask(__name__, static_url_path='', static_folder='../client')
+# This finds exactly where your server.py is sitting
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# This points to the client folder relative to this file
+client_dir = os.path.join(current_dir, '..', 'client')
+
+app = Flask(__name__, static_url_path='', static_folder=client_dir)
 
 util.load_saved_artifacts()
 
-# This route serves your app.html when someone visits the main link
 @app.route('/')
 def index():
-    return send_from_directory('../client', 'app.html')
+    return send_from_directory(client_dir, 'app.html')
+
 @app.route('/get_location_names')
 def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
+    response = jsonify({'locations': util.get_location_names()})
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['POST'])
@@ -29,14 +29,11 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
-    response.headers.add('Access-Control-Allow-origin', '*')
-
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    print("Starting python flask for prediction success")
     app.run(host="0.0.0.0", port=port)
